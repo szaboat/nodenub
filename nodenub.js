cf3ab724f7da48ccd6cb1b53344efa23864ce220
Attila Szabo <attila.szabo@prezi.com>
2012-10-13 22:43:07 +0200

    yt, lfms commands

--- nodenub.js
+++ nodenub.js
@@ -5,7 +5,9 @@ var commands = {
   'g': 'http://www.google.com/search?q=%s',
   'so': 'http://stackoverflow.com/search?q=%s',
   'gim': 'http://images.google.com/images?q=%s',
-  'flk': 'http://www.flickr.com/photos/search/text:%s'
+  'flk': 'http://www.flickr.com/photos/search/text:%s',
+  'yt': 'http://www.youtube.com/results?search_type=search_videos&search_sort=relevance&search_query=%s&search=Search',
+  'lfms': 'http://www.dotblob.com/~szaboat/lastfmsimilar/index.php?artist=%s'
 }
 
 http.createServer(function (req, res) {
