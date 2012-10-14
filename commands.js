296e943f127662fac398dd360516e5d0a773ba02
Attila Szabo <attila.szabo@prezi.com>
2012-10-14 20:51:35 +0200

    szaki commands

--- commands.js
+++ commands.js
@@ -4,5 +4,8 @@ exports.commands = {
   'gim': 'http://images.google.com/images?q=%s',
   'flk': 'http://www.flickr.com/photos/search/text:%s',
   'yt': 'http://www.youtube.com/results?search_type=search_videos&search_sort=relevance&search_query=%s&search=Search',
-  'lfms': 'http://www.dotblob.com/~szaboat/lastfmsimilar/index.php?artist=%s'
+  'lfms': 'http://www.dotblob.com/~szaboat/lastfmsimilar/index.php?artist=%s',
+  'sztaki': 'http://szotar.sztaki.hu/dict_search.php?L=ENG%3AHUN%3AEngHunDict&O=HUN&W=%s',
+  'enhu': 'http://dict.sztaki.hu/dict_search.php?L=ENG%3AHUN%3AEngHunDict&O=ENG&flash=&E=1&in_form=1&W=%s&M=2&P=1&C=1&A=1&T=1&F=0',
+  'huen': 'http://dict.sztaki.hu/dict_search.php?L=HUN%3AENG%3AEngHunDict&O=HUN&flash=&E=1&in_form=1&W=%s&M=2&P=1&C=1&A=1&T=1&F=0'
 }
