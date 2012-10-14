aa439bfc0485dfc19672b7086092ae73418f6063
Attila Szabo <attila.szabo@prezi.com>
2012-10-14 21:40:56 +0200

    Remove console.logs

--- nodenub.js
+++ nodenub.js
@@ -3,14 +3,17 @@ var commands = require('./commands.js').commands;
 var port = process.env.PORT || 6969;
 
 http.createServer(function (req, res) {
-  var sq = require('url').parse(req.url, true).query.cmd.split(' '),
-      cmd = sq[0],
-      cmd_url = sq.splice(1).join('%20');
-      search_url = commands[cmd].replace(/%s/, cmd_url);
+  try {
+    var sq = require('url').parse(req.url, true).query.cmd.split(' '),
+        cmd = sq[0],
+        cmd_url = sq.splice(1).join('%20');
+        search_url = commands[cmd].replace(/%s/, cmd_url);
 
-  console.log('You searched for ' + search_url);
+  } catch (err) {
+    res.writeHead(400);
+    res.end();
+  }
+
   res.writeHead(302, {'Location': search_url});
   res.end();
 }).listen(port, "0.0.0.0");
-
-console.log('Running at ' + port);
