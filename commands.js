5dac5f112a0c45a57738af4081d528d2e814da17
Attila Szabo <attila.szabo@prezi.com>
2012-10-15 13:25:56 +0200

    gi, wp, wphu commands

--- commands.js
+++ commands.js
@@ -1,11 +1,14 @@
 exports.commands = {
-  'g': 'http://www.google.com/search?q=%s',
-  'so': 'http://stackoverflow.com/search?q=%s',
-  'gim': 'http://images.google.com/images?q=%s',
-  'flk': 'http://www.flickr.com/photos/search/text:%s',
-  'yt': 'http://www.youtube.com/results?search_type=search_videos&search_sort=relevance&search_query=%s&search=Search',
-  'lfms': 'http://www.dotblob.com/~szaboat/lastfmsimilar/index.php?artist=%s',
-  'sztaki': 'http://szotar.sztaki.hu/dict_search.php?L=ENG%3AHUN%3AEngHunDict&O=HUN&W=%s',
   'enhu': 'http://dict.sztaki.hu/dict_search.php?L=ENG%3AHUN%3AEngHunDict&O=ENG&flash=&E=1&in_form=1&W=%s&M=2&P=1&C=1&A=1&T=1&F=0',
-  'huen': 'http://dict.sztaki.hu/dict_search.php?L=HUN%3AENG%3AEngHunDict&O=HUN&flash=&E=1&in_form=1&W=%s&M=2&P=1&C=1&A=1&T=1&F=0'
+  'flk': 'http://www.flickr.com/photos/search/text:%s',
+  'g': 'http://www.google.com/search?q=%s',
+  'gi': 'http://images.google.com/images?q=%s',
+  'gim': 'http://images.google.com/images?q=%s',
+  'huen': 'http://dict.sztaki.hu/dict_search.php?L=HUN%3AENG%3AEngHunDict&O=HUN&flash=&E=1&in_form=1&W=%s&M=2&P=1&C=1&A=1&T=1&F=0',
+  'lfms': 'http://www.dotblob.com/~szaboat/lastfmsimilar/index.php?artist=%s',
+  'so': 'http://stackoverflow.com/search?q=%s',
+  'sztaki': 'http://szotar.sztaki.hu/dict_search.php?L=ENG%3AHUN%3AEngHunDict&O=HUN&W=%s',
+  'wp': 'http://en.wikipedia.org/?search=%s',
+  'wphu': 'http://hu.wikipedia.org/?search=%s',
+  'yt': 'http://www.youtube.com/results?search_type=search_videos&search_sort=relevance&search_query=%s&search=Search'
 }
