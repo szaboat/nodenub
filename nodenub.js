725fbaa9cb2e1d95bc040255a1086f3dc8f93e4e
Attila Szabo <attila.szabo@prezi.com>
2014-05-15 22:32:54 +0200

    Require url

--- nodenub.js
+++ nodenub.js
@@ -1,21 +1,22 @@
 var http = require('http');
+var url = require('url');
 var commands = require('./commands.js').commands;
 var port = process.env.PORT || 6969;
 
 http.createServer(function (req, res) {
+  var searchUrl = '';
   try {
-    var sq = require('url').parse(req.url, true).query.cmd.split(' '),
+    var sq = url.parse(req.url, true).query.cmd.split(' '),
         cmd = sq[0],
         cmd_url = sq.splice(1).join('%20');
-        search_url = commands[cmd].replace(/%s/, cmd_url);
-
+    searchUrl = commands[cmd].replace(/%s/, cmd_url);
   } catch (err) {
     res.writeHead(400);
     res.end();
   }
 
-  console.log('search_url ' + search_url);
+  console.log('search_url ' + searchUrl);
 
-  res.writeHead(302, {'Location': search_url});
+  res.writeHead(302, {'Location': searchUrl});
   res.end();
 }).listen(port, "0.0.0.0");
